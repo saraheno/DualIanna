2753686e485b9ceb8ad4386cb3c8ec85e01f194b
Sarah <sarah.eno@cern.ch>
2025-09-10 17:22:47 -0400

    from Ianna

--- compact/digi_dualcrys.py
+++ compact/digi_dualcrys.py
@@ -5,9 +5,7 @@ digi.calCollections = "SimCaloHits"
 digi.outputCalCollection = "DigitizedCaloHits"
 digi.outputRelCollection = "CaloHitLinks"
 digi.CalThreshold = 0.03  # MeV
-digi.calibrationCoeffcal = 120000.0
+# digi.calibrationCoeffcal = 120000.0
 digi.maxCalHitEnergy = 2.0
-digi.detectornameEcal = "DRCrystal"
-digi.detectornameHcal = "DRFtubeFiber"
-
-# Then include this in your ApplicationMgr.TopAlg or ComponentAccumulator if you're using GaudiConfig2
+# digi.detectornameEcal = "DRCrystal"
+# digi.detectornameHcal = "DRFtubeFiber"
