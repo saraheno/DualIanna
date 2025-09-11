9509371b1cec5dec34d179f625a988c8cbe0db8a
Sarah <sarah.eno@cern.ch>
2025-09-11 10:24:25 -0400

    new py

--- compact/digi_dualcrys.py
+++ compact/digi_dualcrys.py
@@ -1,3 +1,17 @@
+from Gaudi.Configuration import *
+from Configurables import ApplicationMgr
+
+from Configurables import k4DataSvc
+dataservice = k4DataSvc("EventDataSvc", input="junk.root")
+
+from Configurables import PodioInput
+podioinput = PodioInput("PodioInput",
+    collections = [
+        "SimCaloHits"
+    ],
+    OutputLevel = DEBUG
+)
+
 from Configurables import DualCrysCalDigi
 
 digi = DualCrysCalDigi("DualCrystalDigitizer")
@@ -5,7 +19,6 @@ digi.calCollections = "SimCaloHits"
 digi.outputCalCollection = "DigitizedCaloHits"
 digi.outputRelCollection = "CaloHitLinks"
 digi.CalThreshold = 0.03  # MeV
-# digi.calibrationCoeffcal = 120000.0
 digi.maxCalHitEnergy = 2.0
-# digi.detectornameEcal = "DRCrystal"
-# digi.detectornameHcal = "DRFtubeFiber"
+
+# Then include this in your ApplicationMgr.TopAlg or ComponentAccumulator if you're using GaudiConfig2
