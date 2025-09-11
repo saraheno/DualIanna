f12d7908c53608c9b8b351e18d46bee6a287556e
Sarah <sarah.eno@cern.ch>
2025-09-11 10:37:10 -0400

    new py

--- compact/digi_dualcrys.py
+++ compact/digi_dualcrys.py
@@ -22,3 +22,29 @@ digi.CalThreshold = 0.03  # MeV
 digi.maxCalHitEnergy = 2.0
 
 # Then include this in your ApplicationMgr.TopAlg or ComponentAccumulator if you're using GaudiConfig2
+from Configurables import PodioOutput
+podiooutput = PodioOutput("PodioOutput", filename = "./yuckyuckyuck_digi.root", OutputLevel = DEBUG)
+podiooutput.outputCommands = ["keep *"]
+
+from Configurables import HepRndm__Engine_CLHEP__RanluxEngine_ as RndmEngine
+rndmEngine = RndmEngine('RndmGenSvc.Engine',
+  SetSingleton = True,
+  Seeds = [ 2345678 ] # default seed is 1234567
+)
+
+from Configurables import RndmGenSvc
+rndmGenSvc = RndmGenSvc("RndmGenSvc",
+  Engine = rndmEngine.name()
+)
+
+
+ApplicationMgr(
+    TopAlg = [
+        podioinput,
+        DualCrysCalDigi,
+        podiooutput
+    ],
+    EvtSel = 'NONE',
+    EvtMax = 10,
+    ExtSvc = [rndmEngine,rndmGenSvc,dataservice]
+)
