ee0db6657426a4bdd6cfab27f565fd2d87e5c196
Thomas Anderson <tba9h@virginia.edu>
2025-11-12 12:47:06 -0500

    Updated convert_to_simple_tree to record all digis collections

--- compact/convert_to_simple_tree.py
+++ compact/convert_to_simple_tree.py
@@ -12,10 +12,6 @@ parser.add_argument('-o','--output', type=str, default = 'edm4hep_plots.root')
 
 
 args = parser.parse_args()
-
-
-
-
 reader = root_io.Reader(args.file)
 
 tf = ROOT.TFile(args.output, 'RECREATE')
@@ -26,47 +22,85 @@ ys = None
 evt = np.array([0])
 ix=np.array([0])
 iy=np.array([0])
+layer=np.array([0])
 print('Reading events')
-ts = None
-tree =  ROOT.TTree("calvision_sim","digigraphs" )
-xbr = None
-ybr = None
-evtbr = None
-ixbr = None
-iybr = None
+
+def buildWaveformTree(treeName, treeComment, sampling, bins):
+    xs = None
+    ys = None
+    evt = np.array([0])
+    ix=np.array([0])
+    iy=np.array([0])
+    layer=np.array([0])
+
+    xbr = None
+    ybr = None
+    evtbr = None
+    ixbr = None
+    iybr = None
+    layerbr = None
+
+    tree = ROOT.TTree(treeName, treeComment)
+    xs = np.array([0.]*bins)
+    for i in range (0,bins):
+        xs[i] = i*sampling
+            
+    ys = np.array([0.]*bins)    
+    xbr = tree.Branch("xs", xs, f"xs[{bins}]/D")
+    ybr = tree.Branch("ys", ys, f"ys[{bins}]/D")
+    evtbr = tree.Branch("event", evt, "event/I")
+    ixbr =  tree.Branch("ix", ix, "ix/I")
+    iybr =  tree.Branch("iy", iy, "iy/I")
+    layerbr = tree.Branch("layer", layer, "layer/I")
+
+    brs = {}
+    brs['xs'] = (xbr,xs)
+    brs['ys'] = (ybr,ys)
+    brs['evt'] = (evtbr,evt)
+    brs['ix'] = (ixbr,ix)
+    brs['iy'] = (iybr,iy)
+    brs['layer'] = (layerbr, layer)
+    
+    return (tree, brs)
+
+
+
+treeNames = ['CalvisionSiPMDigiWaveform','CalvisionSiPMCerenWaveform','CalvisionSiPMScintWaveform']
+sevt =  reader.get("events")[0]
+trees = {}
+
+
+for name in treeNames:
+
+    collection = sevt.get(name)
+    entry =  collection.at(0)
+
+    trees[name] = buildWaveformTree(name,'Digis',entry.getInterval(),entry.amplitude_size())
+
+
+
 for event in reader.get("events"):
 
-    totalcollection = event.get('CalvisionSiPMDigiWaveform')
-
-    if (evt[0] == 0):
-        ts = totalcollection.at(0)
-        sampling = ts.getInterval()
-        xs = np.array([0.]*1024)
-        for i in range (0,1024):
-            xs[i] = i*sampling
-
-        ys = np.array([0.]*1024)
-        xbr = tree.Branch("xs", xs, "xs[1024]/D")
-        ybr = tree.Branch("ys", ys, "ys[1024]/D")
-        evtbr = tree.Branch("event", evt, "event/I")
-        ixbr =  tree.Branch("ix", ix, "ix/I")
-        iybr =  tree.Branch("iy", iy, "iy/I")
+    for name in treeNames:
+        collection = event.get(name)
         
+        tree, brs = trees[name]
+        
+        for s in range(0,collection.size()):
+            ts = collection.at(s)
+            cellID = ts.getCellID()
+            cix = ((0x7f<<3)&cellID)>>3
+            ciy = ((0x7f<<10)&cellID)>>10
+            brs['ix'][1][0] = cix
+            brs['iy'][1][0] = ciy
 
+            wave = np.array(ts.getAmplitude())
+            bincount = ts.amplitude_size()
+            for i in range(0,bincount):
+                brs['ys'][1][i] = wave[i]
+            
 
-
-    for s in range(0,totalcollection.size()):
-        ts = totalcollection.at(s)
-        cellID = ts.getCellID()
-        cix = ((0x7f<<3)&cellID)>>3
-        ciy = ((0x7f<<10)&cellID)>>10
-        ix[0] = cix
-        iy[0] = ciy
-        wave = np.array(ts.getAmplitude())
-        for i in range(0,1024):
-            ys[i] = wave[i]
-
-        tree.Fill()
-    evt[0]+=1
+            tree.Fill()
+        brs['evt'][1][0] +=1
         
 tf.Write()    
