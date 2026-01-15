692041c91f0aaf73ca0d34742bfae3e1aacf3185
Thomas Anderson <tba9h@virginia.edu>
2026-01-15 14:41:35 -0500

    Added layer flag to digi2d example

--- compact/digi2d.py
+++ compact/digi2d.py
@@ -4,11 +4,14 @@ import time
 
 parser = argparse.ArgumentParser('2D Histogram of simple digis tree')
 parser.add_argument('-f', '--file', type=str, default='edm4hep_plots.root', help='file to process')
+parser.add_argument('-l', '--layer', type=int, default = 0, help='Layer to look at')
 args = parser.parse_args()
 
 rdf = RDataFrame("CalvisionSiPMDigiWaveform", args.file)
 
-prof = rdf.Filter("layer==0").Profile2D(("name", "title", 10, 0, 10, 10, 0, 10), "ix", "iy", "ys")
+layerFilter = f'layer=={args.layer}'
+
+prof = rdf.Filter(layerFilter).Profile2D(("name", "title", 10, 0, 10, 10, 0, 10), "ix", "iy", "ys")
 prof.Draw()
 
 time.sleep(5)
